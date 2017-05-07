270d06cccc7a8d696247ddb4b154542b1a950968
Sergio Ramirez <humanrender@Sergios-MacBook.local>
2017-05-07 19:11:29 +0200

    Drum kick

--- app/index.js
+++ app/index.js
@@ -1,5 +1,24 @@
-import Envelope from './lib/envelope.js';
+import Envelope from './lib/Envelope.js';
 
-const envelope = new Envelope();
+const context = new window.AudioContext();
 
-console.log(Envelope);
+const osc = context.createOscillator();
+osc.type = 'sine';
+osc.frequency.value = 50;
+osc.start(0);
+
+const envelope = new Envelope(context, {
+  attack: 0.001,
+  decay: 0.1,
+  sustain: 0,
+  release: 0.25,
+  length: 0.05
+});
+
+osc.connect(envelope.gain);
+envelope.gain.connect(context.destination);
+
+const trigger = document.getElementById('trigger');
+trigger.addEventListener('click', function(){
+  envelope.trigger();
+});
--- app/lib/envelope.js
+++ app/lib/envelope.js
@@ -1,4 +1,42 @@
+import _ from 'lodash';
+
 class Envelope {
+  constructor(context, props) {
+    this.context = context;
+    this.props = props;
+    this.gain = context.createGain();
+    this.gainParams = this.gain.gain;
+    this.gainParams.value = 0;
+  }
+  trigger() {
+    const { attack, decay, sustain, length } = this.props;
+    const time = this.context.currentTime;
+    const startDecayAt = time + attack;
+    
+    this.gainParams.cancelScheduledValues(time);
+    this.gainParams.setValueAtTime(0, time);
+    this.gainParams.linearRampToValueAtTime(1.0, time + attack);
+    this.gainParams.linearRampToValueAtTime(sustain, startDecayAt + decay);
+
+    if (length){
+      setTimeout(() => { this.release(); }, length * 1000);
+    }
+  }
+  release() {
+    console.log('releasing');
+    const time = this.context.currentTime;
+    const gainValue = this.gainParams.value;
+    const { release } = this.props;
+    
+    this.gainParams.cancelScheduledValues(time);
+    this.gainParams.setValueAtTime(gainValue, time);
+    this.gainParams.linearRampToValueAtTime(0, time + release);
+  }
+  toString(time) {
+    const { attack, decay, sustain, release, length } = this.props;
+    
+    return `Envelope (attack: ${attack}, decay: ${decay}, sustain: ${sustain}, release: ${release}, length: ${length})`;
+  }
 }
 
 export default Envelope;
